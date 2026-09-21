fe5b156295603ef54c94bf48485dc9b271ad9ed9
tg321920-netizen <tg321920@gmail.com>
2026-09-20 18:48:46 -0600

    feat: add video metadata types

---
+++ clipforge/types/video.ts
@@ -0,0 +1,16 @@
+export type ProbeMetadata = {
+  durationSeconds: number;
+  width: number;
+  height: number;
+  fps: number;
+  codec: string;
+  container: string;
+  aspectRatio: string;
+};
+
+export type UploadedVideo = ProbeMetadata & {
+  projectId: string;
+  originalName: string;
+  storedName: string;
+  sizeBytes: number;
+};
